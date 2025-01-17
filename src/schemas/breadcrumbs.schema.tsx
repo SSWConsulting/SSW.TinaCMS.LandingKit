8c9feca89ccdfdd2b3644d39feca49167cbae90b
Calinator444 <calinator666@gmail.com>
2025-01-17 17:27:49 +1100

    made breadcrumb schema simpler

--- src/schemas/breadcrumbs.schema.tsx
+++ src/schemas/breadcrumbs.schema.tsx
@@ -1,17 +1,9 @@
-import { Template } from "tinacms";
-import { backgroundSchema } from "./background.schema";
-import { ColorPickerOptions } from "../interfaces/color-options";
-import { defaultBackgroundOptions } from "../internal/default-config/default-bg-options";
 
-const BreadcrumbsSchemaGenerator: (options?: {backgroundColors?: ColorPickerOptions[], previewSrc?: string}) => Template = (options = {}) => {
-    return {
+const BreadcrumbSchema =  {
+      type: "object",
       name: "breadcrumbs",
-      label: "<SSW> Breadcrumbs",
-      ui: {
-        ...(options?.previewSrc ? {previewSrc: options.previewSrc} : {}),
-      },
+      label: "Breadcrumbs",
       fields: [
-        backgroundSchema(options?.backgroundColors ?? defaultBackgroundOptions),
         {
           type: "string",
           label: "Final Breadcrumb",
@@ -19,7 +11,6 @@ const BreadcrumbsSchemaGenerator: (options?: {backgroundColors?: ColorPickerOpti
           description: "The final breadcrumb in the list",
         },
       ],
-  } as Template;
 };
 
-export default BreadcrumbsSchemaGenerator;
+export default BreadcrumbSchema;
