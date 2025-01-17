0d734d6322246368a7b497558ec638220fba8df9
Calinator444 <calinator666@gmail.com>
2025-01-17 17:11:58 +1100

    exporting breacrumb schema

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import Breadcrumbs from './components/breadcrumbs';
 import { CardCarousel } from './components/card-carousel/card-carousel';
 import LogoCarousel from './components/logo-carousel';
-import BreadcrumbsSchemaGenerator from './schemas/breadcrumbs.schema';
+import BreadcrumbSchema from './schemas/breadcrumbs.schema';
 import LogoCarouselSchemaGenerator from './schemas/logo-carousel.schema';
 export {
-  Breadcrumbs, BreadcrumbsSchemaGenerator as BreadcrumbsSchema, CardCarousel,
+  Breadcrumbs, BreadcrumbSchema, CardCarousel,
   LogoCarousel, LogoCarouselSchemaGenerator as LogoCarouselSchema
 };
 
