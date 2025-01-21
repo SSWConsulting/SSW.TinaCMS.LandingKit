1d76714eb3390d267b57b291183614e87586ae33
Calinator444 <calinator666@gmail.com>
2025-01-22 10:10:20 +1100

    added inter fonts to sample project

--- tina-starter/app/layout.tsx
+++ tina-starter/app/layout.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import "ssw-tinacms-landingkit/dist/style.css";
 import "../styles.css";
 
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  variable: "--inter-font",
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["200", "300", "400", "500", "600", "700"],
+});
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.className}`}>
       <body
         style={{
           margin: "3rem",
